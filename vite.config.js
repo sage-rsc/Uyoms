9f9e48715a3522de176edea67035ad9a917d31d0
James Favour <68597611+sage-rsc@users.noreply.github.com>
2026-01-07 02:43:41 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/Uyoms/' : '/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
   },
+  publicDir: 'public',
 })
 
